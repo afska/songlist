4aa091f96454dd68de77ae063235930d3dea466c
Rodrigo Alfonso <rodri042@gmail.com>
2014-02-28 02:05:49 -0300

    Ahora los ids son la concatenación título-artista

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,18 @@
 class Song
 	include Mongoid::Document
+	before_create :assign_id
 
 	field :title, type: String
 	field :author, type: String
 	field :genre, type: String
 	field :submitted_at, type: Date
 	field :user_id, type: String
+
+	#------
+	private
+	#------
+
+	def assign_id
+		self.id = "#{self.title}-#{self.author}"
+	end
 end 
