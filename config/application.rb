aab9d059441eaaae7aeb723483a958dd229660e9
Rodrigo Alfonso <rodri042@gmail.com>
2014-07-17 03:58:55 -0300

    Y ahora?

--- config/application.rb
+++ config/application.rb
@@ -23,25 +23,20 @@ module Songlist
 		# Add Bower components to assets pipeline
 		config.assets.paths << Rails.root.join("vendor", "assets", "components")
 
-		precompiler_things
+		# --------------------------------
+		# Precompiler things for Heroku...
+		# --------------------------------
 
-		#------
-		private
-		#------
+		# We don't want the default of everything that isn't js or css, because it pulls too many things in
+		config.assets.precompile.shift
 
-		# For Heroku...
-		def precompiler_things
-			# We don't want the default of everything that isn't js or css, because it pulls too many things in
-			config.assets.precompile.shift
-
-			# Explicitly register the extensions we are interested in compiling
-			config.assets.precompile.push(Proc.new do |path|
-				File.extname(path).in? [
-					".html", ".erb", ".haml",                 # Templates
-					".png",  ".gif", ".jpg", ".jpeg", ".svg", # Images
-					".eot",  ".otf", ".svc", ".woff", ".ttf", # Fonts
-				]
-			end)
-		end
+		# Explicitly register the extensions we are interested in compiling
+		config.assets.precompile.push(Proc.new do |path|
+			File.extname(path).in? [
+				".html", ".erb", ".haml",                 # Templates
+				".png",  ".gif", ".jpg", ".jpeg", ".svg", # Images
+				".eot",  ".otf", ".svc", ".woff", ".ttf", # Fonts
+			]
+		end)
 	end
 end
