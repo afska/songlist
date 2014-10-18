fd170b0e4826d2b5c48cbe9cb38cb891065a81e5
Rodrigo Alfonso <rodri042@gmail.com>
2014-10-18 17:01:27 -0300

    Chau precompiler hack

--- config/application.rb
+++ config/application.rb
@@ -22,21 +22,5 @@ module Songlist
 
 		# Add Bower components to assets pipeline
 		config.assets.paths << Rails.root.join("vendor", "assets", "components")
-
-		# --------------------------------
-		# Precompiler things for Heroku...
-		# --------------------------------
-
-		# We don't want the default of everything that isn't js or css, because it pulls too many things in
-		config.assets.precompile.shift
-
-		# Explicitly register the extensions we are interested in compiling
-		config.assets.precompile.push(Proc.new do |path|
-			File.extname(path).in? [
-				".html", ".erb", ".haml",                 # Templates
-				".png",  ".gif", ".jpg", ".jpeg", ".svg", # Images
-				".eot",  ".otf", ".svc", ".woff", ".ttf", # Fonts
-			]
-		end)
 	end
 end
